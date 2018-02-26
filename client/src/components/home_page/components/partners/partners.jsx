59524919e0ac79c043b3f61d57618484a227b205
BlueHotDog <BlueHotDog@users.noreply.github.com>
2018-02-26 20:55:43 +0200

    small changes to partner logos

--- client/src/components/home_page/components/partners/partners.jsx
+++ client/src/components/home_page/components/partners/partners.jsx
@@ -2,18 +2,25 @@ import React from 'react';
 
 require('./partners.scss');
 
+function getLogo(name) {
+  return <img src={require(`../../../../images/partners-logos/${name}@2x.png`)} />
+}
 const Partners = () => {
   return (
     <div className={"partners"}>
       <p className={"title"}>השותפים שלנו</p>
       <div className={"partners-list"}>
-        <img src={require('../../../../images/logo-shorashim.png')}/>
-        <img src={require('../../../../images/logo-hatnua.png')}/>
-        <img src={require('../../../../images/logo-hevra.png')}/>
-        <img src={require('../../../../images/logo-lebinui.png')}/>
-        <img src={require('../../../../images/logo-mithabrim.png')}/>
-        <img src={require('../../../../images/logo-sadna.png')}/>
-        <img src={require('../../../../images/logo-sviva.png')}/>
+        {getLogo('hagar')}
+        {getLogo('hatnua')}
+        {getLogo('hevra')}
+        {getLogo('lebinui')}
+        {getLogo('maze9')}
+        {getLogo('mithabrim')}
+        {getLogo('sadna')}
+        {getLogo('shorashim')}
+        {getLogo('shtil')}
+        {getLogo('sviva')}
+        {getLogo('university')}
       </div>
     </div>
   )
